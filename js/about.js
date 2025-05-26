b9216ac504f4c3137ebfb6d1e99ef959c0c69cbd
Tobymaru <150368953+Blemkeid@users.noreply.github.com>
2025-05-26 15:27:14 +0200

    Changed the threshold do 0.7

--- js/about.js
+++ js/about.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const observerOptions = {
         root: null,
         rootMargin: '0px',
-        threshold: 0.5
+        threshold: 0.7
     };
 
     const observerCallback = (entries, observer) => {
