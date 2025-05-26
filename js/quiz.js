be45e82a4eaf0e7e09eee4efbd1236f3aafb25ed
Kim T. <t.kimlan0871@gmail.com>
2025-05-26 15:04:25 +0200

    Opitmization

--- js/quiz.js
+++ js/quiz.js
@@ -3,19 +3,6 @@ const answersQuizQuestions1and2 = {question1 : "B", question2: "[A, B, C]"}
 const wordsIncludedInQuestion3 = ["empathy", "savant", "genius", "immature", "eye contact", "social", "difficulties", "emotionless"]
 
 document.addEventListener("DOMContentLoaded", () => {
-
-    document.querySelectorAll("input[type='radio']").forEach(radio => {
-        radio.checked = false;
-    });
-    
-    document.querySelectorAll("input[type='checkbox']").forEach(checkbox => {
-        checkbox.checked = false;
-    });
-    
-    document.querySelectorAll("input[type='text']").forEach(textInput => {
-        textInput.value = "";
-    });
-
     let buttonSubmit = document.getElementById("submitButtonID");
 
     buttonSubmit.addEventListener("click", () => {
@@ -25,15 +12,6 @@ document.addEventListener("DOMContentLoaded", () => {
         const answerQuestion1 = checkBoxCheckedQuestion1.value;
         console.log("Question 1: " + checkBoxCheckedQuestion1.value);
 
-        // Display the question 2 if a checkbox is checked
-        if(checkBoxCheckedQuestion1.value){
-            const question2 = document.getElementById("question2");
-            question2.style.display = "block";
-
-            const question1 = document.getElementById("question1");
-            question1.style.display = "none";
-        }
-
         // Values for question 2
         const checkBoxCheckedQuestion2 = document.querySelectorAll("input[type='checkbox'][name='question2']:checked");
         let answersQuestion2 = [];
@@ -42,34 +20,14 @@ document.addEventListener("DOMContentLoaded", () => {
             answersQuestion2.push(checkbox.value);
         })
 
-        // Display the question 3 if at least one checkbox is checked
-        if(answersQuestion2.length > 0) {
-            const question3 = document.getElementById("question3");
-            question3.style.display = "block";
-
-            const question2 = document.getElementById("question2");
-            question2.style.display = "none";
-        }
+        console.log("Question 2: " + JSON.stringify(answersQuestion2));
 
         //Value for question 3
         const answerQuestion3 = document.querySelector("input[type='text'][name='question3']").value;
-
-        if(answerQuestion3){
-
-            const question1 = document.getElementById("question1");
-            question1.style.display = "block";
-
-            const question2 = document.getElementById("question2");
-            question2.style.display = "block";
-
-            const question3 = document.getElementById("question3");
-            question3.style.display = "block";
-
-        }
+        console.log("Question 3: " + answerQuestion3.value);
 
         processQuestion1(answerQuestion1);
         processQuestion2(answersQuestion2);
-        processQuestion3(answerQuestion3);
     
     });
 
@@ -120,10 +78,6 @@ const processQuestion1 = (answer) => {
         p.style.fontWeight = "bold";
     }
 
-    p.style.fontSize = "large"
-    p.style.marginLeft = "1%";
-
-
     divChosenQuestion1.appendChild(p);
 }
 
@@ -152,29 +106,8 @@ const processQuestion2 = (answers) => {
             p.style.fontWeight = "bold";
         }
 
-        p.style.fontSize = "large"
-        p.style.marginLeft = "1%";
-
-
-
         divChosenQuestion2.appendChild(p);
     }
 
 
-}
-
-const processQuestion3 = (answer) => {
-    for(let word of wordsIncludedInQuestion3){
-        if(answer.toLowerCase().includes(word.toLocaleLowerCase())){
-            let div = document.getElementById("question3");
-            let p = document.createElement("p");
-            p.textContent = "Correct !";
-            p.style.color = "green";
-            p.style.fontWeight = "bold";
-            p.style.fontSize = "large";
-
-            div.appendChild(p);
-            return;
-        }
-    }
 }
