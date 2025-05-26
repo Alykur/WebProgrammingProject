9e51b74ab81e5386a4cf53279f10e31c5d6c0904
Tobymaru <150368953+Blemkeid@users.noreply.github.com>
2025-05-26 09:51:39 +0200

    Began to do quiz.js

--- js/quiz.js
+++ js/quiz.js
@@ -1,3 +1,33 @@
-let buttonSubmit = document.getElementById("submitButtonID");
 
-buttonSubmit.addEventListener("click", () => {});
+const answersQuizQuestions1and2 = {question1 : "B", question2: "[A, B, C]"}
+const wordsIncludedInQuestion3 = ["empathy", "savant", "genius", "immature", "eye contact", "social", "difficulties", "emotionless"]
+
+document.addEventListener("DOMContentLoaded", () => {
+    let buttonSubmit = document.getElementById("submitButtonID");
+
+    buttonSubmit.addEventListener("click", () => {
+
+        // Value for question 1
+        const checkBoxCheckedQuestion1 = document.querySelector("input[type='radio'][name='question1']:checked")
+        console.log("Question 1: " + checkBoxCheckedQuestion1.value);
+
+        // Values for question 2
+        const checkBoxCheckedQuestion2 = document.querySelectorAll("input[type='checkbox'][name='question2']:checked");
+        let answersQuestion2 = [];
+
+        checkBoxCheckedQuestion2.forEach((checkbox) => {
+            answersQuestion2.push(checkbox.value);
+        })
+
+        console.log("Question 2: " + JSON.stringify(answersQuestion2));
+
+        //Value for question 3
+        const answerQuestion3 = document.querySelector("input[type='text'][name='question3']");
+        console.log("Question 3: " + answerQuestion3.value);
+
+        
+        
+    });
+
+});
+
