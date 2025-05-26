3eb0329c22816364a74d60c680d2fe5467bbf55b
Tobymaru <150368953+Blemkeid@users.noreply.github.com>
2025-05-26 10:21:54 +0200

    Removed previous feedbacks for question 1

--- js/quiz.js
+++ js/quiz.js
@@ -30,6 +30,14 @@ document.addEventListener("DOMContentLoaded", () => {
 
         let divChosenQuestion1;
 
+        // Remove previous feedbacks for question 1
+        for(let letter of ["A", "B", "C"]) {
+            let div = document.getElementById("divQuestion1Answer" + letter);
+            if(div.getElementsByTagName("p").length > 0) {
+                div.getElementsByTagName("p")[0].remove();
+            }
+        }
+
         switch (checkBoxCheckedQuestion1.value) {
             case "A":
                 divChosenQuestion1 = document.getElementById("divQuestion1AnswerA");
@@ -44,6 +52,13 @@ document.addEventListener("DOMContentLoaded", () => {
                 break;  
         }
 
+        // Prevent having multiple Correct or False for the same answers
+        const existingFeedback = divChosenQuestion1.querySelector("p");
+        if (existingFeedback) {
+            existingFeedback.remove();
+        }
+
+
         let p = document.createElement("p");
         if(answerQuestion1 === answersQuizQuestions1and2.question1) {
             p.textContent = "Correct !";
@@ -57,9 +72,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
         divChosenQuestion1.appendChild(p);
     
-
-
-
     });
 
 });
